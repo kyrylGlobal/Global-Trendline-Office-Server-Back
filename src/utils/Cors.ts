2acbb05a721de890991cb040defa3ce5fa5dad1d
kyrylGlobal <kyryl.global@gmail.com>
2022-01-19 14:31:39 +0300

    Updating cors and package.json scripts

--- src/utils/Cors.ts
+++ src/utils/Cors.ts
@@ -4,7 +4,7 @@ import {Request} from 'express';
 class Cors{
     allowDevCors(req: Request, callBack: Function){
         const options: CorsOptions = {};
-        const allowDevOrigins: string[] = ['http://localhost:3000', 'http://192.168.1.150:3000'];
+        const allowDevOrigins: string[] = ['http://localhost:3000', 'http://192.168.1.230:3000'];
 
         if(allowDevOrigins.includes(req.header("Origin") as string)){
             options.origin = true;
