61facb4e300d7fae0e65824fed6e9a1ef2b0b511
Kyrylo Popyk <it.kyryl@gmail.com>
2022-05-10 23:31:08 +0200

    Sending emails WIP

--- src/helpers/mailSender.ts
+++ src/helpers/mailSender.ts
@@ -5,6 +5,10 @@ import csv from 'csv-parser'
 import { on } from 'events';
 import country from '../config/config';
 
+function timeout(ms: any) {
+    return new Promise(resolve => {setTimeout(() => {resolve('');}, ms);});
+}
+
 interface Gmail {
     log: string,
     pass: string,
@@ -14,12 +18,12 @@ interface Gmail {
     content: string
 }
 
-export function sendGmail(data: Gmail) {
+export async function sendGmail(data: Gmail) {
     var transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
-            user: data.log,
-            pass: data.pass
+            user: "kyryl.global@gmail.com",//data.log,
+            pass: "Whatareyoudoingglobal03011973"//data.pass
         }
     })
 
@@ -30,13 +34,13 @@ export function sendGmail(data: Gmail) {
         text: data.content 
     }
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if(error) {
-            console.log(error);
-        } else {
-            console.log(`Mail from ${data.log} was succesfully sended.`);
-        }
-    })
+    let mailSendingResult = await transporter.sendMail(mailOptions);
+
+    if(mailSendingResult.accepted.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 interface BaselinkerMailsData {
@@ -45,47 +49,69 @@ interface BaselinkerMailsData {
     country: string
 }
 
-export function sendMailsFromCSV(filePath: string) {
-    fs.createReadStream(filePath)
-    .pipe(csv())
-    .on('data', (row: BaselinkerMailsData) => {
-        let countryWasFounded = false;
-        for(let contryElement of country) {
-            for(let countryName of contryElement.names) {
-                if(countryName === row.country) {
-                    countryWasFounded = true;
-                    if(true) {
-                        sendGmail({
-                            log: contryElement.supportEmail.log,
-                            pass: contryElement.supportEmail.pas,
-                            from: `EasyShop <${contryElement.supportEmail.log}>`,
-                            to: row.email,
-                            content: `${row.name}, ${contryElement.mailText}`,
-                            title: contryElement.productName
-                        })
-                    }
-                }
 
-                if(countryWasFounded) {
-                    break;
-                }
-            }
-
-            if(countryWasFounded) {
-                break;
-            }
-        }
-    })
-    .on('end', () => {
-        console.log('Done reading!')
+async function getCsvData(filePath: string): Promise<BaselinkerMailsData[]> {
+    return new Promise( (resolve, reject) => {
+        let rows: BaselinkerMailsData[] = [];
+        fs.createReadStream(filePath)
+        .pipe(csv())
+        .on('data', async (row: BaselinkerMailsData) => {
+            rows.push(row);
+        })
+        .on('end', () => {
+            resolve(rows);
+        })
     })
 }
 
+function cleanData(data: BaselinkerMailsData[]) {
+    let cleanedData: BaselinkerMailsData[] = [];
 
-//
-//asparagussgreen@gmail.com
-//kyrylpopyk@gmail.com
-//kyrylpopyk1@gmail.com
-//kyrylpopyk2@gmail.com
-//popykmaksym15@gmail.com
-//sveto4kaaa0312@gmail.com
+    for(let dataElement of data) {
+        let haveCoppy = false;
+        for(let cleanedDataElement of cleanedData) {
+            if(cleanedDataElement.email === dataElement.email) {
+                haveCoppy = true;
+                break;
+            }
+        }
+        if(!haveCoppy) {
+            cleanedData.push(dataElement);
+        }
+    }
+
+    return cleanedData;
+}
+
+export async function sendMailsFromCSV(filePath: string) {
+
+    const mailsData = await getCsvData(filePath);
+    const cleanedData = cleanData(mailsData);
+    let countSendedMails = 0;
+    let sendedMailsCount: any = {};
+    for(let mailData of cleanedData) {
+        for(let countryElement of country) {
+            for(let countryName of countryElement.names) {
+                if(countryName === mailData.country) {
+                    await timeout(150);
+                    console.log("Start sending");
+                    let mailSended = await sendGmail({
+                        log: countryElement.supportEmail.log,
+                        pass: countryElement.supportEmail.pas,
+                        from: `EasyShop <${countryElement.supportEmail.log}>`,
+                        to: mailData.email,
+                        content: `${mailData.name}, ${countryElement.mailText}`,
+                        title: countryElement.productName
+                    })
+                    
+                    if(mailSended) {
+                        countSendedMails++;
+                        console.log(`Mail was sended. nr. ${countSendedMails}`);
+                        sendedMailsCount[mailData.country] = sendedMailsCount[mailData.country] ? sendedMailsCount[mailData.country] + 1 : 1;
+                    }
+                }
+            }
+        }
+    }
+    console.log(sendedMailsCount);
+}
